ca93aec99a1b7202904f97ca682409b03a980567
Gibum <gudeng0611@cau.ac.kr>
2025-10-22 00:57:54 +0900

    feat: create tab components
     tab atom 및 molecule 제작
      tab들을 구성하는 각각의 tab 제작
      tab을 이용해서 챌린지를 선택할 수 있는 tabs 제작

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,5 @@
-import DropdownCategory from '@/components/molecules/DropdownCategory/DropdownCategory';
-import DropdownDocument from '@/components/molecules/DropdownDocument/DropdownDocument';
-import DropdownSort from '@/components/molecules/DropdownSort/DropdownSort';
-
 const Home = () => {
-  return (
-    <div>
-      Category
-      <DropdownCategory />
-      <br />
-      Sort
-      <DropdownSort />
-      <br />
-      Document
-      <DropdownDocument />
-      <br />
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
