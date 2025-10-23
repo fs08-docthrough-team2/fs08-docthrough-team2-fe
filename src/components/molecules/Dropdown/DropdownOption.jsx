298469211f15acf7d38abc1e77a4726ae55d86ff
Gibum <gudeng0611@cau.ac.kr>
2025-10-23 16:31:47 +0900

    feat: create profile dropdown
     프로필을 보여주는 dropdown 생성 및 css 수정
      GNB에서 사용되는 프로필 보여주는 dropdown 생성
      자주쓰는 색상 variable 추가 및 관련 코드 수정

--- src/components/molecules/Dropdown/DropdownOption.jsx
+++ src/components/molecules/Dropdown/DropdownOption.jsx
@@ -7,7 +7,6 @@ import optionTriggerIcon from '/public/icon/ic_option_trigger.svg';
 import styles from '@/styles/components/molecules/Dropdown/DropdownOption.module.scss';
 
 const DROPDOWN_OPTIONS = ['수정하기', '삭제하기'];
-const OPTION_TRIGGER_ARIA_LABEL = '옵션 정렬 열기';
 
 function DropdownOption() {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,7 +24,14 @@ function DropdownOption() {
       <button type="button" className={styles.optionTriggerButton} onClick={handleToggle}>
         <Image src={optionTriggerIcon} alt="option button" width={16} height={16} priority />
       </button>
-      <DropdownList options={DROPDOWN_OPTIONS} isOpen={isOpen} onSelect={handleSelect} />
+      <DropdownList
+        options={DROPDOWN_OPTIONS}
+        isOpen={isOpen}
+        onSelect={handleSelect}
+        listClassName={styles.optionList}
+        listItemClassName={styles.optionListItem}
+        optionClassName={styles.optionButton}
+      />
     </div>
   );
 }
