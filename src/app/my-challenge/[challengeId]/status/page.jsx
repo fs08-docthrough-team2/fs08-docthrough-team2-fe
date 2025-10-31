24c61b62ac6a3b53fd504298793ea0501e9b8527
MinJun916 <ed125248@gmail.com>
2025-10-31 11:42:45 +0900

    feat: build ChallengeApprovalDetailPage frame

--- src/app/my-challenge/[challengeId]/status/page.jsx
+++ src/app/my-challenge/[challengeId]/status/page.jsx
@@ -1,7 +1,29 @@
+import styles from '@/styles/pages/my-challenge/ChallengeApprovalDetailPage.module.scss';
+
+import ChallengeApprovalStatus from '@/components/atoms/ChallengeApprovalStatus/ChallengeApprovalStatus';
 import ChallengeCardDetail from '@/components/molecules/ChallengeCard/ChallengeCardDetail';
+import LinkPreview from '@/components/atoms/LinkPreview/LinkPreview';
 
 const ChallengeApprovalDetailPage = () => {
-  return <ChallengeCardDetail isMyChallenge={true} />;
+  const stroke = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="891" height="1" viewBox="0 0 891 1" fill="none">
+      <path d="M0.5 0.5H890.5" stroke="#E5E5E5" strokeLinecap="round" />
+    </svg>
+  );
+  return (
+    <div className={styles.page}>
+      <ChallengeApprovalStatus />
+      <div className={styles.stroke}>{stroke}</div>
+      <div className={styles.content}>
+        <ChallengeCardDetail isMyChallenge={true} />
+        {stroke}
+        <div className={styles.description}>
+          <div className={styles.title}>원문 링크</div>
+          <LinkPreview url="https://github.com/fs08-docthrough-team2/fs08-docthrough-team2-fe" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChallengeApprovalDetailPage;
