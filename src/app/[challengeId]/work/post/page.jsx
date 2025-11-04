4cdf18474517b847700966bcb919eb62b1180b73
MinJun916 <ed125248@gmail.com>
2025-11-04 14:36:25 +0900

    feat: add temporary save modal notification to WorkPostPage

--- src/app/[challengeId]/work/post/page.jsx
+++ src/app/[challengeId]/work/post/page.jsx
@@ -1,20 +1,43 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 import WorkPost from '@/components/templates/WorkPost';
+import Button from '@/components/atoms/Button/Button';
 
 import styles from '@/styles/pages/work/WorkPostPage.module.scss';
+import ic_cancel from '/public/icon/ic_cancel.svg';
 
 const WorkPostPage = () => {
   const [content, setContent] = useState('');
+  const [isDraftOpen, setIsDraftOpen] = useState(true);
+
+  const isHasDraft = true;
 
   const handleContentChange = (value) => {
     setContent(value);
   };
 
+  const handleDraftOpen = () => {
+    setIsDraftOpen((prev) => !prev);
+  };
+
   return (
     <div className={styles.page}>
       <WorkPost content={content} onContentChange={handleContentChange} />
+      {isHasDraft && isDraftOpen && (
+        <div className={styles.draft}>
+          <div className={styles.left}>
+            <button className={styles.cancelButton} onClick={handleDraftOpen}>
+              <Image src={ic_cancel} alt="cancel" width={24} height={24} />
+            </button>
+            <div className={styles.description}>
+              임시 저장된 작업물이 있어요. 저장된 작업물을 불러오시겠어요??
+            </div>
+          </div>
+          <Button variant="solid" size="sm" children="불러오기" />
+        </div>
+      )}
     </div>
   );
 };
