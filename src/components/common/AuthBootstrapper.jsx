214abbcb96864fe11b23e65d4f157346223f150a
MinJun916 <ed125248@gmail.com>
2025-11-06 15:22:32 +0900

    fix: resolve GNB error caused by backend response structure change

--- src/components/common/AuthBootstrapper.jsx
+++ src/components/common/AuthBootstrapper.jsx
@@ -24,7 +24,7 @@ const AuthBootstrapper = () => {
         }
 
         const res = await api.get('/user/my');
-        const user = res.data?.data;
+        const user = res.data?.data?.user;
 
         if (user) setUser(user);
       } catch (error) {
