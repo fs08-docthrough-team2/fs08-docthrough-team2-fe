b3c2a2322b96fe61c4e5bda08a961a71745223d0
MinJun916 <ed125248@gmail.com>
2025-11-10 00:52:03 +0900

    feat: 어드민 작업물 페이지 로딩 처리 추가

--- src/app/admin/[challengeId]/work/[workId]/page.jsx
+++ src/app/admin/[challengeId]/work/[workId]/page.jsx
@@ -4,29 +4,35 @@ import { useParams } from 'next/navigation';
 import Work from '@/components/templates/Work';
 import { useGetChallengeDetail } from '@/hooks/queries/useChallengeQueries';
 import { useGetChallengeWorkDetail } from '@/hooks/queries/useChallengeWorkQueries';
+import Spinner from '@/components/common/Spinner';
 
 import styles from '@/styles/pages/admin/AdminWorkPage.module.scss';
 
 const AdminWorkPage = () => {
   const { challengeId, workId } = useParams();
-  const { data: challengeDetail } = useGetChallengeDetail(challengeId);
-  const { data: challengeWorkDetail } = useGetChallengeWorkDetail(workId);
+  const { data: challengeDetail, isLoading: isChallengeDetailLoading } =
+    useGetChallengeDetail(challengeId);
+  const { data: challengeWorkDetail, isLoading: isChallengeWorkDetailLoading } =
+    useGetChallengeWorkDetail(workId);
 
   return (
-    <div className={styles.page}>
-      <Work
-        isAdmin={true}
-        title={challengeDetail?.data?.title}
-        type={challengeDetail?.data?.field}
-        category={challengeDetail?.data?.type}
-        nickName={challengeWorkDetail?.data?.item?.nickName}
-        createdAt={challengeWorkDetail?.data?.item?.createdAt}
-        likeCount={challengeWorkDetail?.data?.item?.likeCount}
-        workItem={challengeWorkDetail?.data?.item?.workItem}
-        attendId={workId}
-        likeByMe={challengeWorkDetail?.data?.item?.likeByMe}
-      />
-    </div>
+    <>
+      {<Spinner isLoading={isChallengeDetailLoading || isChallengeWorkDetailLoading} />}
+      <div className={styles.page}>
+        <Work
+          isAdmin={true}
+          title={challengeDetail?.data?.title}
+          type={challengeDetail?.data?.field}
+          category={challengeDetail?.data?.type}
+          nickName={challengeWorkDetail?.data?.item?.nickName}
+          createdAt={challengeWorkDetail?.data?.item?.createdAt}
+          likeCount={challengeWorkDetail?.data?.item?.likeCount}
+          workItem={challengeWorkDetail?.data?.item?.workItem}
+          attendId={workId}
+          likeByMe={challengeWorkDetail?.data?.item?.likeByMe}
+        />
+      </div>
+    </>
   );
 };
 
